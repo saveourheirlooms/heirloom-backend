bc9658706da176302e40af35a6bb47db63e191d2
Murat Dogan <46164888+doganmurat@users.noreply.github.com>
2021-08-28 00:09:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.options('/nfts', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.end();
+});
 app.use(cors());
 app.use(express.json());
 app.use(function(req, res, next) {
