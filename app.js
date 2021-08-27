b7a27a76c91ae64a1a66b402985fe75b86c2fbeb
Murat Dogan <46164888+doganmurat@users.noreply.github.com>
2021-08-27 23:55:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "saveourheirlooms.github.io");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 
